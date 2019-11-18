78df442b5a996214248009ab593327eb9c68b8f0
Matthew Stinis <mpstinis@gmail.com>
2019-11-18 13:33:02 -0500

    Created Singup component + store mod
    Added action to be handled in reducer of store.js. Created Signup component, includes form and handlers.

---
+++ frontend/src/Signup.jsx
@@ -0,0 +1,58 @@
+import React, { Component } from "react";
+import { connect } from "react-redux";
+
+class UnconnectedSignup extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      username: "",
+      password: ""
+    };
+  }
+
+  handleUsernameChange = event => {
+    console.log("new username", event.target.value);
+    this.setState({ username: event.target.value });
+  };
+
+  handlePasswordChange = event => {
+    console.log("new password", event.target.value);
+    this.setState({ password: event.target.value });
+  };
+
+  handleSubmit = async event => {
+    event.preventDefault();
+    console.log("Signup form submitted");
+    let data = new FormData();
+    data.append("username", this.state.username);
+    data.append("password", this.state.password);
+    let response = await fetch("/signup", {
+      method: "POST",
+      body: data,
+      credentials: "include"
+    });
+    let responseBody = await response.text();
+    let body = JSON.parse(responseBody);
+    if (!body.success) {
+      window.alert("Username in use");
+      return;
+    }
+    this.props.dispatch({ type: "login-success" });
+  };
+
+  render = () => {
+    return (
+      <form onSubmit={this.handleSubmit}>
+        Username
+        <input type="text" onChange={this.handleUsernameChange} />
+        Password
+        <input type="text" onChange={this.handlePasswordChange} />
+        <input type="submit" />
+      </form>
+    );
+  };
+}
+
+let Signup = connect()(UnconnectedSignup);
+
+export default Signup;
