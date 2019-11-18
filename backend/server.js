dfac1411c68434a3b852796e72b799a77164052d
Xigrux <xing.jingyuan@outlook.com>
2019-11-17 23:53:47 -0500

    connected to mongo database

--- backend/server.js
+++ backend/server.js
@@ -2,6 +2,8 @@
 let express = require("express");
 let multer = require("multer");
 let cors = require("cors");
+let MongoClient = require("mongodb").MongoClient;
+let ObjectID = require("mongodb").ObjectID;
 
 //=============================== INITIALIZE LIBRARIES ===============================//
 let app = express();
@@ -13,10 +15,35 @@ app.use("/uploads", express.static("uploads"));
 // setting for cors, do not touch, frontend is on localhost3000
 app.use(cors({ credentials: true, origin: "http://localhost:3000" }));
 
+// settings for mongo
+let dbo = undefined;
+let url =
+  "mongodb+srv://lulul:123@cluster0-jjd2c.mongodb.net/test?retryWrites=true&w=majority";
+MongoClient.connect(url, { useNewUrlParser: true }, (err, db) => {
+  dbo = db.db("alibay");
+});
+
 //=============================== GET ENDPOINTS ===============================//
 app.get("/test", (req, res) => {
   res.send("Backend connected");
 });
+
+app.get("/testdb", (req, res) => {
+  dbo
+    .collection("test")
+    .find({ _id: ObjectID("5dd21fe11c9d44000054cab8") })
+    .toArray((error, test) => {
+      if (error) {
+        res.send(JSON.stringify({ success: false }));
+      } else {
+        res.send(JSON.stringify(test));
+      }
+    });
+});
+
+//=============================== POST ENDPOINTS ===============================//
+
+//add here
 
 //=============================== LISTENER ===============================//
 app.listen("4000", () => {
