6fc024d7fb0b279c31b99a18d2d3d0c5a3199620
Matthew Stinis <mpstinis@gmail.com>
2019-11-19 15:09:53 -0500

    Update Cart.jsx
    added methods, mapState, pulling data from server to display relevant fields. Needs testing

--- frontend/src/Cart.jsx
+++ frontend/src/Cart.jsx
@@ -1,10 +1,47 @@
 import React, { Component } from "react";
 import { connect } from "react-redux";
+import { Link } from "react-router-dom";
 
 class UnconnectedCart extends Component {
-  render = () => {};
+  getCartItems = async () => {
+    let data = new FormData();
+    data.append("cart", this.props.itemIds);
+    let response = await fetch("/cart", {
+      method: "POST",
+      body: data
+    });
+    let responseBody = await response.text();
+    let parsedBody = JSON.parse(responseBody);
+    return parsedBody;
+  };
+  render = () => {
+    let cart = this.getCartItems();
+    let items = this.props.itemIds.map(itemId, index => {
+      let quantityToBuy = 0;
+      cart.forEach(item => {
+        if (item.productName === cart[index].productName) {
+          quantityToBuy++;
+        }
+      });
+      return (
+        <div>
+          <div>{cart[index].productName}</div>
+          <div>Quantity: {quantityToBuy}</div>
+          <div>
+            <Link to={"/product/" + itemId} />
+          </div>
+          <div>{cart[index].descriptionBody}</div>
+        </div>
+      );
+    });
+    return { items };
+  };
 }
 
-let Cart = connect()(UnconnectedCart);
+let mapStateToProps = state => {
+  itemIds: state.cart;
+};
+
+let Cart = connect(mapStateToProps)(UnconnectedCart);
 
 export default Cart;
