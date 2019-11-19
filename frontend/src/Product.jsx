dedc1829d7ed23ab43af8d8b87a33f7003b2bd24
Matthew Stinis <mpstinis@gmail.com>
2019-11-19 11:51:19 -0500

    Updated product.jsx
    Add proper handlers, corrected state, and form inputs. Needs testing

--- frontend/src/Product.jsx
+++ frontend/src/Product.jsx
@@ -6,15 +6,15 @@ class UnconnectedProduct extends Component {
     super(props);
     this.state = {
       productName: "",
-      sellerName: "", // FIX ME
-      productDesc: "",
+      sellerName: this.props.merchant.username, // FIX ME
+      productDescHeader: "",
+      productDescText: "",
       productCat: "",
       assocTags: [],
       productLocation: "",
-      productImages: [], // FIX ME
       inventory: 0,
       dateOfPublication: "",
-      rating: 0
+      files: ""
     };
   }
 
@@ -24,9 +24,13 @@ class UnconnectedProduct extends Component {
     console.log("new product name: ", event.target.value);
     this.setState({ productName: event.target.value });
   };
-  handleDescriptionChange = event => {
+  handleDescHeaderChange = event => {
     console.log("new description: ", event.target.value);
-    this.setState({ productDesc: event.target.value });
+    this.setState({ productDescHeader: event.target.value });
+  };
+  handleDescInfoChange = event => {
+    console.log("new description: ", event.target.value);
+    this.setState({ productDescText: event.target.value });
   };
   handleCategoryChange = event => {
     console.log("new category: ", event.target.value);
@@ -34,16 +38,14 @@ class UnconnectedProduct extends Component {
   };
   handleTagChange = event => {
     console.log("new tags: ", event.target.value);
-    this.setState({ assocTags: event.target.value }); // push to array
+    this.setState({
+      assocTags: event.target.value
+    }); // push to array
   };
   handleLocationChange = event => {
     console.log("new location: ", event.target.value);
     this.setState({ productLocation: event.target.value });
   };
-  // handleImagesChange = event => {
-  //   console.log("new product name", event.target.value);
-  //   this.setState({ productName: event.target.value });
-  // };
   handleInventoryChange = event => {
     console.log("product inventory: ", event.target.value);
     this.setState({ inventory: event.target.value });
@@ -52,25 +54,28 @@ class UnconnectedProduct extends Component {
     console.log("publication date: ", event.target.value);
     this.setState({ dateOfPublication: event.target.value });
   };
-  handleRatingChange = event => {
-    console.log("new rating: ", event.target.value);
-    this.setState({ rating: event.target.value });
+  handleFileChange = event => {
+    console.log("new files: ", event.target.files);
+    this.setState({ files: event.target.files });
   };
 
   // Form submission
 
   handleSubmit = async event => {
     event.preventDefault();
+    let tags = this.state.assocTags.split(",");
     let data = new FormData();
     data.append("name", this.state.productName);
-    data.append("desc", this.state.productDesc);
+    data.append("sellerName");
+    data.append("description-header", this.state.productDescHeader);
+    data.append("description-text", this.state.productDescText);
     data.append("category", this.state.productCat);
-    data.append("tags", this.state.assocTags);
+    data.append("tags", tags);
     data.append("location", this.state.productLocation);
     data.append("inventory", this.state.inventory);
-    data.append("publicationDate", this.state.dateOfPublication);
-    data.append("rating", this.state.rating);
-    let response = await fetch("/FIXME", {
+    data.append("date", this.state.dateOfPublication);
+    data.append("media", this.state.files);
+    let response = await fetch("/post-item", {
       // fix fetch request path
       method: "POST",
       body: data
@@ -90,22 +95,54 @@ class UnconnectedProduct extends Component {
       <div>
         <h2>New Product</h2>
         <form onSubmit={this.handleSubmit}>
-          <span>Name</span>
-          <input type="text" onChange={this.handleNameChange} />
-          <span>Description</span>
-          <input type="text" onChange={this.handleDescriptionChange} />
-          <span>Category</span>
-          <input type="text" onChange={this.handleCategoryChange} />
-          <span>Tags</span>
-          <input type="text" onChange={this.handleTagChange} />
-          <span>Location</span>
-          <input type="text" onChange={this.handleLocationChange} />
-          <span>Inventory</span>
-          <input type="text" onChange={this.handleInventoryChange} />
-          <span>Publication date</span>
-          <input type="text" onChange={this.handleDateChange} />
-          <span>Rating</span>
-          <input type="text" onChange={this.handleRatingChange} />
+          {/* NAME */}
+          <label htmlFor="prod-name">Name</label>
+          <input id="prod-name" type="text" onChange={this.handleNameChange} />
+          {/* DESC */}
+          <label htmlFor="prod-desc-header">Description header</label>
+          <input
+            id="prod-desc-header"
+            type="text"
+            onChange={this.handleDescHeaderChange}
+          />
+          <label htmlFor="prod-desc-text">Description text</label>
+          <input
+            id="prod-desc-text"
+            type="text"
+            onChange={this.handleDescTextChange}
+          />
+          {/* CATEGORY */}
+          <label htmlFor="prod-category">Category</label>
+          <input
+            id="prod-category"
+            type="text"
+            onChange={this.handleCategoryChange}
+          />
+          {/* TAGS */}
+          <label htmlFor="prod-tags">Tags</label>
+          <input id="prod-tags" type="text" onChange={this.handleTagChange} />
+          {/* LOCATION */}
+          <label htmlFor="prod-location">Location</label>
+          <input
+            id="prod-location"
+            type="text"
+            onChange={this.handleLocationChange}
+          />
+          {/* INVENTORY */}
+          <label htmlFor="prod-inventory">Inventory</label>
+          <input
+            id="prod-inventory"
+            type="text"
+            onChange={this.handleInventoryChange}
+          />
+          {/* IMAGES */}
+          <label htmlFor="prod-images">Product images</label>
+          <input
+            id="prod-images"
+            type="file"
+            onChange={this.handleFileChange}
+            multiple
+          />
           <input type="submit" value="Submit product" />
         </form>
       </div>
