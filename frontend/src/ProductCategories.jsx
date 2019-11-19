c03ace5fff502fdd2e41858ecdde013595e4215a
melansonS <samuelmelanson@live.com>
2019-11-19 16:46:07 -0500

    Added the product categories component
    added the filtering logic

--- frontend/src/ProductCategories.jsx
+++ frontend/src/ProductCategories.jsx
@@ -33,24 +33,86 @@ let dummyDataItems = [
     rating: "3"
   }
 ];
+//gets all of the tags from the each of the items
+let allTags = [];
+dummyDataItems.forEach(item => {
+  item.tags.forEach(tag => {
+    allTags.push(tag);
+  });
+});
+//filters out the duplicate values so as to have unique tag buttons
+let filteredTags = [];
+allTags.forEach(tag => {
+  if (!filteredTags.includes(tag)) {
+    filteredTags.push(tag);
+  }
+});
 
 class UnconnectedProductCategories extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      filters: []
+    };
+  }
+  handleTagUpdate = event => {
+    //if innactive, set the class to active and add the value to the filters array in the state
+    if (event.target.className === "innactive") {
+      event.target.className = "active";
+      this.setState({ filters: this.state.filters.concat(event.target.value) });
+    }
+    // if already active, set the calss to innactive and filter out that tag from the filters array in the state
+    else if (event.target.className === "active") {
+      event.target.className = "innactive";
+      this.setState({
+        filters: this.state.filters.filter(tag => {
+          return tag !== event.target.value;
+        })
+      });
+    }
+  };
+
   render = () => {
+    let tagButtons = filteredTags.map(tag => {
+      //"-" in place temporarily to visually differenciate the buttons
+      return (
+        <button
+          value={tag}
+          className="innactive"
+          onClick={this.handleTagUpdate}
+        >
+          {tag} -
+        </button>
+      );
+    });
+    //Creates a product card component for each of the Items in the items array
     let itemCards = dummyDataItems.map(item => {
       return (
         <ProductCard
           id={item.id}
           title={item.title}
           frontEndPath={item.frontEndPath}
+          tags={item.tags}
           description={item.description}
           price={item.price}
           rating={item.rating}
         ></ProductCard>
       );
     });
+
+    // if there are filters in place, modify the itemCards array
+    if (this.state.filters[0] !== undefined) {
+      console.log("All active filters", this.state.filters);
+      this.state.filters.forEach(tag => {
+        itemCards = itemCards.filter(itemCard => {
+          return itemCard.props.tags.includes(tag);
+        });
+      });
+    }
     return (
       <div>
-        {/* <Navbar></Navbar>  navbar has yet to be implemented*/}
+        <Navbar></Navbar>
+        {tagButtons}
         {itemCards}
       </div>
     );
