3854192299f38e4ad7d0de11f0afc9fd46013ede
Xigrux <xing.jingyuan@outlook.com>
2019-11-22 12:36:53 -0500

    styling cart

--- frontend/src/Cart.jsx
+++ frontend/src/Cart.jsx
@@ -3,7 +3,8 @@ import { connect } from "react-redux";
 import { Link } from "react-router-dom";
 
 import ProductCard from "./ProductCard.jsx";
-
+import RemoveItem from "./RemoveItem.jsx";
+import "./style/cart.css";
 class UnconnectedCart extends Component {
   constructor(props) {
     super(props);
@@ -30,75 +31,55 @@ class UnconnectedCart extends Component {
     if (cart.length > 0) {
       this.setState({ displayItems: await this.getCartItems(cart) });
     }
-    console.log(this.state.displayItems);
   };
 
   getCartItems = async cart => {
     cart = cart.join(",");
-    console.log("cart before fetch", cart);
+
     let data = new FormData();
     data.append("cart", cart);
+
     let response = await fetch("/cart", {
       method: "POST",
       body: data
     });
+
     let responseBody = await response.text();
     let parsedBody = JSON.parse(responseBody);
-    console.log("cart after fetch", parsedBody);
+
     return parsedBody.items;
   };
-
-  removeDupes = arr => {
-    let seen = {};
-    let ret = [];
-    for (let i = 0; i < arr.length; i++) {
-      let item = arr[i].productName; // item = array's individual obj's product name
-      if (!seen[item]) {
-        // if the array's obj's name prop isn't in seen obj, or has value of false...
-        seen[item] = true; // the item is now considered seen
-        ret.push(arr[i]); // push entire object to new array
-      }
-    }
-    return ret;
-  };
   render = () => {
-    // let items = this.props.itemIds.map((itemId, index) => {
-    //   let quantityToBuy = 0;
-
-    //   cart.forEach(item => {
-    //     if (item.productName === cart[index].productName) {
-    //       quantityToBuy++;
-    //     }
-    //   });
-    //   return (
-    //     <div>
-    //       <div>{cart[index].productName}</div>
-    //       <div>Quantity: {quantityToBuy}</div>
-    //       <div>
-    //         <Link to={"/product/" + itemId}>View item</Link>
-    //       </div>
-    //       <div>{cart[index].descriptionBody}</div>
-    //     </div>
-    //   );
-    // });
-
-    // return (
-    //   <div>
-    //     {items}
-    //     <div>
-    //       cart <Link to="/checkout">checkout</Link>
-    //     </div>
-    //   </div>
-    // ); // TODO: filter items to prevent duplicates from being displayed
     if (
       this.state.displayItems &&
       this.props.isLoggedIn &&
       this.props.cart.length > 0
     ) {
       console.log(this.state.displayItems);
-      return this.state.displayItems.map(o => {
-        return <ProductCard itemContents={o}></ProductCard>;
-      });
+      let itemQty;
+      return (
+        <>
+          <div>YOUR CART</div>
+          {this.state.displayItems.map(o => {
+            return (
+              <div class="cart-item-container">
+                <div class="cart-item">
+                  <ProductCard itemContents={o}></ProductCard>
+                  <RemoveItem />
+                </div>
+
+                {this.props.cart.forEach(item => {
+                  if (o._id === item.itemId) {
+                    itemQty = item.quantity;
+                  }
+                })}
+
+                <div class="cart-item-quantity">{itemQty}</div>
+              </div>
+            );
+          })}
+        </>
+      );
     }
 
     return <>your cart is empty</>;
