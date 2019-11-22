91298e710e46091a043670c0c94c3e55e218da18
Xigrux <xing.jingyuan@outlook.com>
2019-11-22 15:05:37 -0500

    added cart remove

--- backend/cart.js
+++ backend/cart.js
@@ -2,16 +2,10 @@ let ObjectID = require("mongodb").ObjectID;
 
 let addToCart = (req, res, dbo) => {
   let update = JSON.parse(req.body.update);
-  console.log("what is update??? ", update);
-  console.log("what TYPE is update??? ", typeof update);
-  console.log("in add cart");
   let itemId = req.body.productId;
-  console.log("item id:", itemId);
   let userId = req.body.userId;
-  console.log("user id:", userId);
   let quantity = parseInt(req.body.quantity);
   let item = { itemId, quantity };
-  console.log("updated cart: ", item);
   if (update) {
     dbo
       .collection("users")
@@ -33,15 +27,33 @@ let addToCart = (req, res, dbo) => {
 };
 
 let removeFromCart = (req, res, dbo) => {
-  let itemId = req.body.itemNo;
+  console.log("in removeFromCart");
+  let update = JSON.parse(req.body.update);
+  let quantity = parseInt(req.body.quantity);
   let userId = req.body.userId;
-  let cart = req.body.cart;
-  let i = cart.indexOf(itemId);
-  cart.splice(i, 1);
-  dbo
-    .collection("users")
-    .updateOne({ _id: ObjectID(userId) }, { $set: { cart } });
-  return res.send(JSON.stringify({ success: true, cart }));
+  let itemId = req.body.productId;
+  console.log("in update", update);
+  console.log("in quantity", quantity);
+  console.log("in userId", userId);
+  console.log("in itemId", itemId);
+  let item = { itemId, quantity };
+
+  if (update) {
+    dbo
+      .collection("users")
+      .updateOne(
+        { _id: ObjectID(userId), "cart.itemId": itemId },
+        { $inc: { "cart.$.quantity": quantity * -1 } }
+      );
+
+    return res.send(JSON.stringify({ success: true, item }));
+  } else {
+    dbo
+      .collection("users")
+      .updateOne({ _id: ObjectID(userId) }, { $pull: { cart: item } });
+
+    return res.send(JSON.stringify({ success: true, item }));
+  }
 };
 
 let cart = (req, res, dbo) => {
--- frontend/src/RemoveItem.jsx
+++ frontend/src/RemoveItem.jsx
@@ -10,18 +10,72 @@ class UnconnectedRemoveItem extends Component {
       quantitySelected: 1
     };
   }
-  handleAddToCart = async event => {};
+  removeItem = async event => {
+    event.preventDefault();
+    let inCartQty = parseInt(this.props.itemContents.quantity);
+    let update = false;
+    if (inCartQty - this.state.quantitySelected < 1) {
+      update = false;
+      console.log("remove it");
+    } else {
+      update = true;
+      console.log("now is ", inCartQty - this.state.quantitySelected);
+    }
+
+    let data = new FormData();
+    data.append("update", update);
+    data.append("quantity", this.state.quantitySelected);
+    data.append("userId", this.props.user._id);
+    console.log(this.props.itemContents.itemId);
+    data.append("productId", this.props.itemContents.itemId);
+
+    let response = await fetch("/remove-from-cart", {
+      method: "POST",
+      body: data
+    });
+    let responseBody = await response.text();
+    let parsedBody = JSON.parse(responseBody);
+
+    if (!parsedBody.success) {
+      window.alert("Product submission failed");
+    } else {
+      this.props.cart.forEach(i => {
+        if (i.itemId === parsedBody.item.itemId) {
+          console.log("matching", i.itemId, "and ", parsedBody.item.itemId);
+          console.log(typeof this.state.quantitySelected);
+
+          i.quantity = i.quantity - parseInt(this.state.quantitySelected);
+
+          return;
+        }
+      });
+
+      console.log("this is the Response item", parsedBody.item);
+
+      console.log("this is the new cart obj", this.props.cart);
+
+      this.props.dispatch({
+        type: "add-cart",
+        cart: this.props.cart.splice(
+          0,
+          this.props.cart.length,
+          ...this.props.cart
+        )
+      });
+    }
+  };
 
   handleQuantity = e => {
     e.preventDefault();
-    this.setState({ quantitySelected: e.target.value });
+    console.log(e.target.value);
+    this.setState({ quantitySelected: parseInt(e.target.value) });
   };
   render() {
     return (
       <>
         {this.props.isLoggedIn && this.props.user.userType === "users" && (
           <div class="removeitem-container">
-            <button onClick={this.handleAddToCart}>
+            <button onClick={this.removeItem}>
               <Link>
                 {/* <Link to="/cart"> */}
                 REMOVE
